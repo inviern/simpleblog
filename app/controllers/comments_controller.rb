dfe7c9fe47ce2aac5c0d9ddea10e870adba1edaf
nika <inviern@gmail.com>
2016-07-07 15:26:51 +0300

    implement Comments controller create, destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,36 @@
 class CommentsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :author_user, only: [:destroy]
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post_id = params[:post_id]
+    @comment.author = current_user
+    if @comment.save
+      flash[:success] = 'Comment added'
+      redirect_to blog_post_path(@comment.post.author.name, @comment.post)
+    else
+      render 'posts/show', author: @comment.post.author, post: @comment.post
+    end
+  end
+
+  def destroy
+    if @comment.destroy
+      flash[:success] = 'Comment deleted'
+      redirect_to blog_post_path(@comment.post.author.name, @comment.post)
+    else
+      render 'posts/show', author: @comment.post.author, post: @comment.post
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+  def author_user
+    @comment = Comment.find(params[:id])
+    redirect_to root_path unless current_user == @comment.author
+  end
 end
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Sorcery::TestHelpers::Rails::Integration
+  include Sorcery::TestHelpers::Rails::Controller
+
+  def setup
+    @comment = comments(:first)
+    @user = users(:first)
+    @another_user = users(:second)
+    @post = posts(:first)
+  end
+
+  # create
+  test 'authorized users should create comments' do
+    login_user(@user, login_path)
+    assert_difference 'Comment.count', 1 do
+      post :create, post_id: @post, comment: { text: 'text' }
+    end
+  end
+
+  test 'non-authorized users should not create comments' do
+    post :create, post_id: @post
+    assert_redirected_to login_path
+  end
+
+  # destroy
+  test 'authorized users should destroy their comments' do
+    login_user(@comment.author, login_path)
+    assert_difference 'Comment.count', -1 do
+      delete :destroy, id: @comment
+    end
+  end
+
+  test 'authorized users should not destroy other user comments' do
+    login_user(@another_user, login_path)
+    assert_no_difference 'Comment.count' do
+      delete :destroy, id: @comment
+    end
+  end
+
+  test 'non-authorized users should not destroy comments' do
+    delete :destroy, id: @comment
+    assert_redirected_to login_path
+  end
 end
