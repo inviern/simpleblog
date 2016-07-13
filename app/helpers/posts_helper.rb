ecdf23b398ea83305d99cb22c7393c8ddd313ce9
nika <inviern@gmail.com>
2016-07-13 10:31:37 +0300

    add truncate text on home page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  def formatted_post_text(post, options = { truncate: false })
+    formatted_text = post.text
+
+    if options[:truncate]
+      formatted_text = truncate(post.text, length: 600, separator: ' ') do
+        link_to ' Read more', blog_post_path(post.author.name, post)
+      end
+    end
+
+    simple_format(formatted_text)
+  end
 end
